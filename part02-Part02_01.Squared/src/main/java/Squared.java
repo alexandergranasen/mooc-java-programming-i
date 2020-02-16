036a94877a638f246e3d46d48f5fcf28d9864cec
Alexander Granåsen <alexander.granasen@gmail.com>
2020-02-16 21:19:35 +0100

    Completed part 2, exercise 1

--- part02-Part02_01.Squared/src/main/java/Squared.java
+++ part02-Part02_01.Squared/src/main/java/Squared.java
@@ -5,6 +5,9 @@ public class Squared {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Please write an number: ");
+        int number = Integer.valueOf(scanner.nextLine());       
+        int total = number*number;
+        System.out.println(total);
     }
 }
