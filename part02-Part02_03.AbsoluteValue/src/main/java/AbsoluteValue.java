a362161eceebfd93ccd85c3a81fd83f6864555d2
Alexander Granåsen <alexander.granasen@gmail.com>
2020-02-20 21:35:51 +0100

    Completed part 2, exercise 3

--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -6,5 +6,13 @@ public class AbsoluteValue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int maxValue = Integer.parseInt(scanner.nextLine());
+        if(maxValue < 0) {
+            System.out.println(maxValue*-1);
+        } else {
+            System.out.println(maxValue*1);
+        }
+        
+        
     }
 }
