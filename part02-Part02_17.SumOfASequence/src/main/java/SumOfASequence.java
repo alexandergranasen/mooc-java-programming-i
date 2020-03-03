c0fcd1df448e588412b3f8d43a4e0bfedfce3c46
Alexander Granåsen <alexander.granasen@gmail.com>
2020-03-03 18:12:35 +0100

    Completed part 2, exercise 17

--- part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -1,10 +1,22 @@
 
+import java.awt.BorderLayout;
 import java.util.Scanner;
 
 public class SumOfASequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        int number = 0;
+        System.out.println("Last number?");
+        number = Integer.valueOf(scanner.nextLine());
+        
+        int sum = 0;
+        for(int i = 0; i <= number; i++){
+            sum += i;
+        }
+        
+        System.out.println("The sum is " +sum);
+        
     }
 }
