3dad01cae9576de444fc12725b813f9c9b17cbfb
Alexander <alexander.granasen@gmail.com>
2020-03-11 11:02:13 +0100

    Completed part 2, exercise 34

--- part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -3,35 +3,57 @@ public class AdvancedAstrology {
 
     public static void printStars(int number) {
         // part 1 of the exercise
-        for(int i = 0; i < number; i++){
+        int i = 0;
+        while (i < number) {
             System.out.print("*");
+            i++;
         }
-        System.out.println(" ");
+        System.out.println("");
     }
-
+ 
     public static void printSpaces(int number) {
         // part 1 of the exercise
-        for(int i = number; i > 0; i--){
-            System.out.print("1 ");
+        int i = 0;
+        while (i < number) {
+            System.out.print(" ");
+            i++;
         }
     }
-
+ 
     public static void printTriangle(int size) {
         // part 2 of the exercise
-        for(int i = 0; i < 4; i++){
-            printSpaces(size);
-            printStars(size);
-            System.out.println("");
+        // let's start from 1 because the first line should have one star!
+        int i = 1;
+        while (i <= size) {
+            printSpaces(size - i);
+            printStars(i);
+            i++;
         }
     }
-
+ 
     public static void christmasTree(int height) {
         // part 3 of the exercise
+        // Let's first print the top of the tree. 
+        // This works almost like the triangle.
+        int i = 1;
+        while (i <= height) {
+            printSpaces(height - i);
+            printStars(i + (i - 1));
+            i++;
+        }
+ 
+        // End then the bottom.
+        printSpaces(height - 2);
+        printStars(3);
+        printSpaces(height - 2);
+        printStars(3);
     }
 
-    public static void main(String[] args) {
-        // The tests are not checking the main, so you can modify it freely.
 
+    public static void main(String[] args) {
+        
+        printStars(3);
+        System.out.println("---");
         printTriangle(4);
         System.out.println("---");
         christmasTree(4);
