920686d38d9068d91e4b3043f567bbaa2dd53aee
Alexander Granåsen <alexander.granasen@gmail.com>
2020-12-05 11:15:41 +0100

    Completed part 3, exercise 29

--- part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class FirstWords {
 
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-    while(true){
-        String word = scanner.nextLine();
-        if(word.equals("")){
-            break;
-        }
+        while(true){
+            String word = scanner.nextLine();
+            if(word.equals("")){
+                break;
+            }
 
-        String[] words = word.split(" ");
-        System.out.println(words[0]);
+            String[] words = word.split(" ");
+            System.out.println(words[0]);
         }
     }
 }
--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -5,7 +5,15 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        while(true){
+            String word = scanner.nextLine();
+            if(word.equals("")){
+                break;
+            }
 
-
+            String[] words = word.split(" ");
+            System.out.println(words[words.length-1]);
+        }
     }
 }
