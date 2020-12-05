8553d8bc7f688829916caf680957bb8cbaea1a97
Alexander Granåsen <alexander.granasen@gmail.com>
2020-12-05 12:53:31 +0100

    Completed part 3, exercise 32

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,38 +5,23 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        int sum = 0;
-        int[] ages = new int[50];
-        int count = 0;
+        int age, oldestAge = 0;
 
         while (true) 
         {
-            try 
-            {
-                String input = reader.nextLine();
-                if (input.equals("")) 
-                {
-                break;
-                }
+            String input = reader.nextLine();
             String[] parts = input.split(",");
-            sum = sum + Integer.valueOf(parts[1]);
-            ages[count] = Integer.valueOf(parts[1]);
-            count = count + 1;    
-            } catch(IndexOutOfBoundsException e) 
+            if (input.equals("")) 
             {
-                System.out.println("You have to enter 'name', 'age' then press enter.");
+            break;
             }
-        }  
         
-        int oldestAge = 0;
-        
-        for(int i = 0; i < ages.length; i++)
-        {
-            if(ages[i] >= oldestAge)
+            age = Integer.valueOf(parts[1]);
+            if (age >= oldestAge) 
             {
-                oldestAge = ages[i];
-            }
-        }
+                oldestAge = age;
+            }  
+       }
         System.out.println("The oldest age in the group is: " +oldestAge);
-    }
+    }   
 }
--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,42 +5,25 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        int sum = 0;
-        int[] ages = new int[50];
-        String[] names = new String[50];
-        int count = 0;
+        int age, oldestAge = 0;
+        String helperName = "";
 
         while (true) 
         {
-            try 
-            {
-                String input = reader.nextLine();
-                if (input.equals("")) 
-                {
-                break;
-                }
+            String input = reader.nextLine();
             String[] parts = input.split(",");
-            sum = sum + Integer.valueOf(parts[1]);
-            names[count] = parts[0];
-            ages[count] = Integer.valueOf(parts[1]);
-            count = count + 1;    
-            } catch(IndexOutOfBoundsException e) 
+            if (input.equals("")) 
             {
-                System.out.println("You have to enter 'name', 'age' then press enter.");
+            break;
             }
-        }  
         
-        int oldestAge = 0;
-        String oldestName = "";
-        
-        for(int i = 0; i < ages.length; i++)
-        {
-            if(ages[i] >= oldestAge)
-            {
-                oldestAge = ages[i];
-                oldestName = names[i];
+            age = Integer.valueOf(parts[1]);
+            if (age >= oldestAge) {
+                oldestAge = age;
+                helperName = parts[0];
             }
         }
-        System.out.println("The oldest age in the group is: " + oldestName);
-    }
+        System.out.println("Name of the oldest: " + helperName);
+    }   
 }
+        
