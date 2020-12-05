6b48f9a086c43ff53350e13f32bb91c2e6bc1130
Alexander Granåsen <alexander.granasen@gmail.com>
2020-12-05 12:11:29 +0100

    Completed part 3, exercise 31

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -4,8 +4,43 @@ import java.util.Scanner;
 public class NameOfTheOldest {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner reader = new Scanner(System.in);
+        int sum = 0;
+        int[] ages = new int[50];
+        String[] names = new String[50];
+        int count = 0;
 
-
+        while (true) 
+        {
+            try 
+            {
+                String input = reader.nextLine();
+                if (input.equals("")) 
+                {
+                break;
+                }
+            String[] parts = input.split(",");
+            sum = sum + Integer.valueOf(parts[1]);
+            names[count] = parts[0];
+            ages[count] = Integer.valueOf(parts[1]);
+            count = count + 1;    
+            } catch(IndexOutOfBoundsException e) 
+            {
+                System.out.println("You have to enter 'name', 'age' then press enter.");
+            }
+        }  
+        
+        int oldestAge = 0;
+        String oldestName = "";
+        
+        for(int i = 0; i < ages.length; i++)
+        {
+            if(ages[i] >= oldestAge)
+            {
+                oldestAge = ages[i];
+                oldestName = names[i];
+            }
+        }
+        System.out.println("The oldest age in the group is: " + oldestName);
     }
 }
