77bb8c9bbff6d93d02ac86987f120b738b78657b
Alexander Granåsen <alexander.granasen@gmail.com>
2020-12-06 13:11:57 +0100

    Completed part 4, exercise 1-8

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,11 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account alexandersAccount = new Account("Alexander Granåsen", 100.0);
+        alexandersAccount.deposit(20);
+        System.out.println(alexandersAccount);
+    }
+}
